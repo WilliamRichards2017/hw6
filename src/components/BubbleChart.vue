1b35257798f749bcc106030da217fa2caaad98c7
Will Richards <Trill@wills-mbp-2.lan>
2019-10-16 23:34:43 -0600

    working toggle button, scaling got messed up with plot init

--- src/components/BubbleChart.vue
+++ src/components/BubbleChart.vue
@@ -1,6 +1,10 @@
 <template>
     <div id="bubbleChart">
 
+
+
+        <button v-on:click="separate = !separate"></button>
+
         <svg width="1000" height="1000"></svg>
 
 
@@ -8,6 +12,7 @@
 </template>
 
 <script>
+
 
     export default {
         name: 'BubbleChart',
@@ -19,117 +24,122 @@
             return {
                 margin: {top: 40, right: 40, bottom: 40, left: 40},
 
+                separate: false,
+
+                minTotal: null,
+                maxTotal: null,
+
+                radiusScale: null,
 
                 colorDict: {
                     "education": "green",
-                    "economy/fiscal issues" : "red",
-                    "crime/justice" : "blue",
-                    "mental health/substance abuse" : "purple",
-                    "health care" : "pink",
-                    "energy/environment":  "magenta",
+                    "economy/fiscal issues": "red",
+                    "crime/justice": "blue",
+                    "mental health/substance abuse": "purple",
+                    "health care": "pink",
+                    "energy/environment": "magenta",
                 },
 
                 yDict: {
                     "education": 0,
-                    "economy/fiscal issues" : 100,
-                    "crime/justice" : 200,
-                    "mental health/substance abuse" : 300,
-                    "health care" : 400,
-                    "energy/environment":  500,
+                    "economy/fiscal issues": 100,
+                    "crime/justice": 200,
+                    "mental health/substance abuse": 300,
+                    "health care": 400,
+                    "energy/environment": 500,
                 }
-
 
 
             }
         },
 
         methods: {
-            categoryToColor(category){
+            categoryToColor(category) {
 
-            if(this.colorDict.hasOwnProperty(category)){
-                return this.colorDict[category];
-            }
-            return "grey";
+                if (this.colorDict.hasOwnProperty(category)) {
+                    return this.colorDict[category];
+                }
+                return "grey";
 
-          },
+            },
+
+            initScales() {
+
+                let self = this;
+
+                self.minTotal = d3.max(self.words, function (d) {
+                    return +d.total;
+                });
+                self.maxTotal = d3.max(self.words, function (d) {
+                    return -d.total;
+                });
+
+                self.radiusScale = d3.scaleLinear()
+                    .domain([self.minTotal, self.maxTotal])
+                    .range([1, 12]);
+            },
+
 
             buildBubbleChart() {
 
                 let self = this;
-
                 let data = self.words;
-
-
-
-                let maxTotal = d3.max(data, function(d) { return +d.total;} );
-                let minTotal = d3.max(data, function(d) { return -d.total;} );
-
-
-
-                console.log("maxTotal", maxTotal);
-                console.log("minTotal", minTotal);
-
-
-
-
-                let radiusScale = d3.scaleLinear()
-                    .domain([minTotal, maxTotal])
-                    .range([1, 12]);
-
-
-
-                let svg = d3.select('#bubbleChart > svg');
 
 
                 d3.select('#bubbleChart > svg')
                     .selectAll('circle')
                     .data(data)
                     .join('circle')
-                    .attr('r', d => radiusScale(d.total))
-                    .attr('cx', function(d) {
+                    .attr('cx', function (d) {
+                        console.log("d", d.category);
+                        return d.sourceX;
+                    })
+                    .attr('cy', function (d) {
+                        return d.sourceY + 100;
+                    })
+                    .attr('r', d => self.radiusScale(d.total))
+                    .style("fill", d => self.colorDict[d.category]);
+
+            },
+
+            separateBubbleChart() {
+
+                let self = this;
+                let data = self.words;
+
+                d3.select('#bubbleChart > svg')
+                    .selectAll('circle')
+                    .data(data)
+                    .join('circle')
+                    .attr('cx', function (d) {
                         console.log("d", d.category);
                         return d.moveX;
                     })
-                    .attr('cy', function(d) {
+                    .attr('cy', function (d) {
                         return d.moveY + 100;
                     })
                     .style("fill", d => self.categoryToColor(d.category));
+            },
 
-                // var simulation = d3.forceSimulation(data)
-                //     .force('charge', d3.forceManyBody().strength(1))
-                //     .force('center', d => d3.forceCenter(d.sourceX, d.sourceY))
-                //     .force('collision', d3.forceCollide().radius(function(d) {
-                //         return 5
-                //     })).on('tick', ticked());
-
-
-
-
-                // function ticked() {
-                //
-                //     d3.select('#bubbleChart > svg')
-                //         .selectAll('circle')
-                //         .data(data)
-                //         .join('circle')
-                //             .attr('r', 5)
-                //             .attr('cx', function(d) {
-                //             return d.sourceX;
-                //         })
-                //         .attr('cy', function(d) {
-                //             return d.sourceY;
-                //         })
-                //
-                // }
-
-
-
-
-            }
 
         },
 
+
+        watch: {
+
+            separate: function () {
+
+                if (this.separate) {
+                    this.separateBubbleChart();
+                } else {
+                    this.buildBubbleChart();
+                }
+            }
+        },
+
         mounted() {
-            this.buildBubbleChart()
+            this.initScales();
+            this.buildBubbleChart();
         }
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { ToggleButton } from 'vue-js-toggle-button'
+
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+
+Vue.component('ToggleButton', ToggleButton)
+
+

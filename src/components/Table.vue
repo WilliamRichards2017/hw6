046ec340ccdc3655d3a9dfd993720c993de4b572
Will Richards <Trill@Wills-MacBook-Pro-2.local>
2019-10-18 16:58:13 -0600

    styling freq bars by category

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -27,13 +27,31 @@
         data() {
             return {
 
+                colorDict: {
+                    "education": "green",
+                    "economy/fiscal issues": "red",
+                    "crime/justice": "blue",
+                    "mental health/substance abuse": "purple",
+                    "health care": "pink",
+                    "energy/environment": "magenta",
+                },
+
             }
         },
 
         methods: {
 
+            categoryToColor(category) {
+
+                if (this.colorDict.hasOwnProperty(category)) {
+                    return this.colorDict[category];
+                }
+                return "grey";
+
+            },
+
             cellArray(d){
-                return [{"type": "phrase", "val" : d.phrase}, {"type" : "freq", "val" : d.total}, {"type" : "percent", "val" : [d.percent_of_d_speeches, d.percent_of_r_speeches]}, {"type" : "total", "va" :d.total}]
+                return [{"type": "phrase", "val" : d.phrase}, {"type" : "freq", "val" : [d.total, d.category]}, {"type" : "percent", "val" : [d.percent_of_d_speeches, d.percent_of_r_speeches]}, {"type" : "total", "val" :d.total}]
             },
 
             buildTable() {
@@ -49,10 +67,49 @@
                     return d.type === 'phrase'
                 });
 
+                let total = td.filter((d) => {
+                    return d.type === 'total'
+                });
+
+                let percent = td.filter((d) => {
+                    return d.type === 'percent'
+                });
+
+                let freq = td.filter((d) => {
+                    return d.type === 'freq'
+                });
+
                 console.log("phrases", phrases);
 
-                phrases.append("text")
+                phrases.join("text")
                     .text(d => {console.log("d.val", d.val); return d.val});
+
+                total.join("text")
+                    .text(d => d.val);
+
+                let freqWidth = 100;
+
+
+
+                let freqSvgs = freq.append("svg")
+                    .attr("class", "chart")
+                    .attr("width", freqWidth)
+                    .attr("height", 25);
+
+
+                let freqScale = d3.scaleLinear()
+                    .domain([0, 1])
+                    .range([10, freqWidth-10]);
+
+                let barHeight = 20;
+
+                freqSvgs
+                    .append("rect")
+                    .attr("width", d => freqScale(d.val[0]/50))
+                    .attr("height", barHeight)
+                    .style("fill", d => this.categoryToColor(d.val[1]))
+
+
 
             },
 
